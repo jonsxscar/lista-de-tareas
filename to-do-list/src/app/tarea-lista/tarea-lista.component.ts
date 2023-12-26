67738319d88d0a520cb055a64685f11443fcac13
jonsxscar <jonsxscar@gmail.com>
2023-12-25 22:06:23 -0500

    lista tarea beta

--- to-do-list/src/app/tarea-lista/tarea-lista.component.ts
+++ to-do-list/src/app/tarea-lista/tarea-lista.component.ts
@@ -1,13 +1,30 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'tarea-lista',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './tarea-lista.component.html',
-  styleUrl: './tarea-lista.component.css'
+  styleUrl: './tarea-lista.component.css',
 })
 export class TareaListaComponent {
-  
+  tareas = [
+    { nombre: 'Tarea 1', completada: false },
+    { nombre: 'Tarea 2', completada: true },
+    { nombre: 'Tarea 3', completada: false },
+  ];
+  nuevaTarea = '';
+
+  agregarTarea() {
+    if (this.nuevaTarea) {
+      this.tareas.push({ nombre: this.nuevaTarea, completada: false });
+      this.nuevaTarea = '';
+    }
+  }
+
+  eliminarTarea(index: number) {
+    this.tareas.splice(index, 1);
+  }
 }
