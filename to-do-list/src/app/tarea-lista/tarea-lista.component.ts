3f0a78367f67e5d1cb4cbc4f5393a4474f9e01ba
jonsxscar <jonsxscar@gmail.com>
2023-12-25 21:18:11 -0500

    componente angular

---
+++ to-do-list/src/app/tarea-lista/tarea-lista.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tarea-lista',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tarea-lista.component.html',
+  styleUrl: './tarea-lista.component.css'
+})
+export class TareaListaComponent {
+  
+}
